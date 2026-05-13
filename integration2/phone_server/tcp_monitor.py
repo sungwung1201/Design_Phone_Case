4b19467fcc8ce8dd97d532d6a7a4e085122376ad
4반_07166_박지언 <pigrachel70@gmail.com>
2026-05-13 10:24:14 +0900

    Update tcp_monitor.py

--- integration2/phone_server/tcp_monitor.py
+++ integration2/phone_server/tcp_monitor.py
@@ -3,14 +3,26 @@ from rclpy.node import Node
 from sensor_msgs.msg import JointState
 import sqlite3
 import os
+import requests
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_NAME = os.path.join(BASE_DIR, "database.db")
 
 
+def send_log(msg, level="info"):
+    try:
+        requests.post(
+            "http://127.0.0.1:5000/api/robot_logs",
+            json={"message": msg, "level": level},
+            timeout=1
+        )
+    except:
+        pass
+
 class TCPMonitor(Node):
     def __init__(self):
         super().__init__('tcp_monitor')
+        self.log_counter = 0
 
         # 🔥 ROS 구독 (수정됨)
         self.subscription = self.create_subscription(
@@ -36,6 +48,7 @@ class TCPMonitor(Node):
         self.conn.commit()
 
         self.get_logger().info("🔥 JointState → SQLite 저장 시작")
+        send_log("✅ TCPMonitor 시작됨", "info")
 
     def callback(self, msg):
         data = msg.position
@@ -46,6 +59,9 @@ class TCPMonitor(Node):
         )
         self.conn.commit()
 
+        self.log_counter += 1
+        if self.log_counter % 50 == 0:  # ✅ 50번에 1번만 로그 전송
+            send_log(f"관절값 수신 중... {data}", "info")
         self.get_logger().info(f"저장됨 → {data}")
 
 
